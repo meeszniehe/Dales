7515b8ab6533dc124ed1334a126d9e3f5f6a6711
meeszniehe <meesz@niehe.com>
2024-03-27 10:05:53 +0100

    - Added dragforce for SFS_TKE - Cleanup code

--- src/modtrees.f90
+++ src/modtrees.f90
@@ -1,10 +1,7 @@
 !> \file modtrees.f90
 !! By Meesz Niehe, email: meesz@niehe.com, TU Delft, section Atmospheric Physics, date 
 
-! First feedback Steven
-! Github test
 module modtrees
-    ! Using tree data module
     use modtreesdata, only : lapply_trees, lreadfile_trees, ltree_stem, C_stem, A_stem !, ltree_leaves
     use modprecision        
     implicit none
@@ -13,7 +10,6 @@ module modtrees
 
     contains
         subroutine inittrees
-        !!! INITIALIZATION !!!
         ! Global variables,itot=xdim, imax=itot/nprocx, kmax=maxheightvaluedimensions(given)
         ! zh=thickness half-level, zf=thickness at full level
         ! ifinput/ifnamopt are integer number to organise file by numbers
@@ -26,12 +22,6 @@ module modtrees
                                 D_MPI_ALLREDUCE, mpi_max, MPI_SUM
 
         real(field_r), allocatable :: tree_height(:,:)              !< 2D array to store tree heights at each grid point (x,y), field_r precision?
-        
-        !< Number of grid points in a slab excluding obstacles, and the number of obstacle points
-        !integer, allocatable    :: Nairl_trees(:)
-        !integer, allocatable    :: Nair_trees(:) !SvdL, 20231218: veranderd in Nair_trees omdat naam Nair in conflict zou kunnen komen met Nair (modibm). Uiteindelijk moet afhankelijk van gebruik modules Nair = som(Nai_apart) genomen worden.
-        !< for calculating free spaces of air 
-
         integer                 :: i, j, k, ierr, ii, jj, kk, n     !< initialize integer to loop over
         integer, allocatable    :: kindex_stem(:,:)                      !< index of stem height
         character(100)          :: readstring                       !< read files as text
@@ -64,23 +54,18 @@ module modtrees
         endif
 
         !!! ALLOCATE VARIABLES !!!  
-        !write(6,*) 'allocating fields in modtrees'
         allocate(tree_height(itot+1,jtot+1))                ! +1=extra room to store bc or staggered variables, velocity on face/pressure in center?
         allocate(ltree_stem(2-ih:i1+ih,2-jh:j1+jh,k1))      ! 'true' means there is a stem
         allocate(kindex_stem(2-ih:i1+ih,2-jh:j1+jh))
         !allocate(ltree_leaves(2-ih:i1+ih,2-jh:j1+jh,k1))   ! 'true' means leaves
 
-        !allocate(Nair_trees(k1))                                  ! k1=kmax+1
-        !allocate(Nairl_trees(k1))
-
-        ! Set standard value to zero
-        tree_height(:,:) = 0 ! choose value higher than ground and lower than lowest tree
- 
+        ! Set default values to zero and false
+        tree_height(:,:) = 0
         ltree_stem(:,:,:) = .false.
         kindex_stem(:,:) = 0
         !ltree_leaves (:,:,:) = .false.
         
-        !!! 1D input -> 2D Array !!!
+        !!! 1D input -> 2D tree_height map !!!
         if(myid==0) then
             if (lreadfile_trees) then
                 write(6,*) 'Reading inputfile in modtrees'
@@ -110,18 +95,38 @@ module modtrees
         endif !myid==0
         
         call D_MPI_BCAST(tree_height,(itot+1)*(jtot+1),0,comm3d,mpierr)
-        !write(6,*) 'broadcasted tree_height'
 
         !!! INDICATE STEM & LEAF CELLS !!!
         do i=2,i1 ! i1=imax+1
             do j=2,j1
                 do k=1,kmax !
                     if(zf(k).LE.tree_height(i+myidx*imax,j+myidy*jmax)) then  ! obstacle height is above mid point of vertical grid
-                        ltree_stem(i,j,k) = .true.         ! true/false array to indicate stem cells
-                        kindex_stem(i,j)   = k + 1     	! werkt niet voor overhangende bladeren/takken
-                        ! 
+                        !!!! Tree is represented by one straight line going straight up !!!!
+                        ltree_stem(i,j,k) = .true.     ! true/false array to indicate stem cells
+                        kindex_stem(i,j)   = k + 1     ! werkt niet voor overhangende bladeren/takken
                         write(6,*) 'ltree_stem',i+myidx*imax,j+myidy*jmax,i,j,k,ltree_stem(i,j,k),tree_height(i+myidx*imax,j+myidy*jmax),zh(kindex_stem(i,j))
-                        !write(6,*) 'indicated stem and leaf cells'
+                        
+                        ! !!!! Divide tree in smaller stem and thicker crown, same C_d value !!!!
+                        ! if (k <= 2) then ! For the lowest two levels, thickness is 1 grid cell
+                        !     startIdx = 0
+                        !     endIdx = 0
+                        ! else ! For higher levels, thickness is 3 grid cells
+                        !     startIdx = -1
+                        !     endIdx = 1
+                        ! endif 
+                        ! do di = startIdx, endIdx
+                        !     do dj = startIdx, endIdx
+                        !         ! Check boundaries to prevent out-of-bound errors
+                        !         if (i+di >= 2 .AND. i+di <= imax .AND. j+dj >= 2 .AND. j+dj <= jmax) then ! for i=2 en i+di=1, moet die 1 niet de laatste cel van het naastgelegen gebied zijn?
+                        !             ltree_stem(i+di,j+dj,k) = .true.
+                        !             ! Only update kindex_stem for the central cell
+                        !             if (di == 0 .AND. dj == 0) then
+                        !                 kindex_stem(i,j) = k + 1
+                        !             endif
+                        !         endif
+                        !     end do
+                        ! end do
+
                     endif
                 end do  !k
             end do      !j
@@ -129,35 +134,8 @@ module modtrees
 
         ! from ltree_stem data exchange in x,y,z dimension, 2->i1, 2->j1, 1->k1, 
         call excjs(ltree_stem,2,i1,2,j1,1,k1,ih,jh)       
-        !write(6,*) 'excjs ltree_stem succesfull'
-        
-        !!! COUNT NUMBER OF GRIDCELLS WITHOUT TREE PER VERTICAL LEVEL !!!
-        ! Voor statistiek gebruikt, niet voor simuleren
-        !Nair_trees(:) = 0 
-        !Nairl_trees(:) = 0    ! local counter
-        !do i=2,i1
-        !    do j=2,j1
-        !        do k=kindex_stem(i,j),k1          
-        !            Nairl_trees(k) = Nairl_trees(k)+1 
-        !        enddo
-        !    enddo
-        !enddo
-        !write(6,*) 'Statistics done' 
-         
-        ! In d_mpi_allreduce k1 is the count which differs per i,j position due to a changing kindex_stem
-        !call D_MPI_ALLREDUCE(Nairl_trees,Nair_trees,k1,MPI_SUM,comm3d,mpierr)
-        !write(6,*) 'MPI_ALLREDUCE succesfull'
-
-        !write(6,* ) 'start deallocate'
         deallocate(tree_height)
-        !write(6,* ) 'deallocated tree_height'
-        !deallocate(Nairl_trees)
-        !write(6,* ) 'deallocated Nairl_trees'
-        !deallocate(Nair_trees)
-        !write(6,* ) 'deallocated Nair_trees'
         deallocate(kindex_stem)
-        !write(6,* ) 'deallocated kindex_stem'
-        !write(6,*) 'exit initrees'
 
         return ! why? !SvdL, 20231218: ik weet het niet zeker, de fortran beschrijving op internet is er ook niet heel duidelijk over. In feite sluit je hiermee de subroutine af en geef je controle terug aan de routine erboven, maar het statement END SUBROUTINE zou in principe hetzelfde al moeten doen. Dus het lijkt me dubbelop. 
     end subroutine inittrees
@@ -168,8 +146,6 @@ module modtrees
     
         if (.not. (lapply_trees)) return
         deallocate(ltree_stem)
-        !write(6,* ) 'deallocated ltree_stem'
-        !deallocate(kindex_stem) ! deze is niet gealloceerd in hoofd routine, dus kan niet hier gedeallocate worden, maar onderaan inittrees?
         !deallocate(ltree_leaves)
 
         return
@@ -178,45 +154,42 @@ module modtrees
     subroutine applytrees
         ! no use of: thl=liquid potential temperature, qt=total specific humidity, e12=sqrt(tke), sv=scalar variance
         use modglobal,      only:   kmax, i1, j1, k1, ih, jh, dx, dy, dzh, dzf, rdt, timee     ! rdt=timeintegrationinterval, timee=elapsed time since start
-        use modfields,      only:   um, vm, wm, &   !t-1
-                                    u0, v0, w0, &   !t
-                                    up, vp, wp   !tendency of ..m
+        use modfields,      only:   um, vm, wm, e12m&   !t-1
+                                    u0, v0, w0, e120&   !t
+                                    up, vp, wp, e12p    !tendency of ..m
         use modtreesdata,   only:   lapply_trees, C_stem, A_stem
         use modmpi,         only:   excjs    
         use modprecision,   only:   field_r
     
         ! Declare local variables
         integer :: i, j, k
-        real :: drag_stem_u, drag_stem_v
+        real :: drag_stem_u, drag_stem_v 
 
-        !real(field_r) :: ...   
-
-        !write(6,* ) 'starting with applytrees after module and variable declarations'
         if (.not. lapply_trees) return
     
         !!! IMPLEMENTATION DRAG FORCE !!!
-        ! Runge Kutta coefficients? (rk3step, rk3coef, rk3coefi)
-    
-        !SvdL, 20231218: ik snap je berekening/formule hier niet helemaal (gebruik rdt, etc). Morgen bespreken. Code technisch werkt het waarschijnlijk wel.
-
         do i=2,i1
             do j=2,j1
                 do k=1,kmax                 
                     if(ltree_stem(i,j,k)) then   ! could be faster by limiting k to highest tree value?
-                        write(6,*) 'ltree is true for index', i, j, k
-                        ! Calculate drag in centre of cell in u and v direction
+                        write(6,*) 'ltree is true for index', i, j, k                        
+                        ! Drag on resolved TKE
                         drag_stem_u = 0 
                         drag_stem_v = 0
                         call drag_force_stem(C_stem, A_stem, u0(i-1,j,k), v0(i,j-1,k), u0(i,j,k), v0(i,j,k), drag_stem_u, drag_stem_v)
                         ! Reassign the velocity value at the faces adjusted for drag in u and v direction
-                        up(i-1,j,k) = up(i-1,j,k) - drag_stem_u/2        ! averaged for gridspacing dx, up = in kracht uitgedrukt, dx weg?
+                        up(i-1,j,k) = up(i-1,j,k) - drag_stem_u/2        ! both sides get half the drag calculated from the middle. 
                         up(i,j,k) = up(i,j,k) - drag_stem_u/2  
-                        vp(i,j-1,k) = vp(i,j-1,k) - drag_stem_v/2        ! averaged for gridspacing dy
+                        vp(i,j-1,k) = vp(i,j-1,k) - drag_stem_v/2      
                         vp(i,j,k) = vp(i,j,k) - drag_stem_v/2
+                        
+                        ! Drag on SFS-TKE
+                        drag_SFS = 0
+                        call drag_force_SFS_TKE(C_stem, A_stem, u0(i-1,j,k), v0(i,j-1,k), u0(i,j,k), v0(i,j,k), e120, drag_SFS) ! e120?? 
+                        e12p(i,j,k) = e12p(i,j,k) - drag_SFS/2
+                
                         !wp(i,j,k-1) = 0
                         !wp(i,j,k) = 0
-                        ! write(6,*) 'up', up(i-1,j,k), up(i,j,k)
-                        ! write(6,*) 'vp', vp(i,j-1,k), vp(i,j,k)
                     !elseif (ltree_leaves(i,j,k)) then   ! Drag force due to leaves
                         !call drag_force_leaves(C_leaves, A_leaves, um(i,j,k), vm(i,j,k), wm(i,j,k), drag_leaves_u, drag_leaves_v, drag_leaves_w)
                         !up(i,j,k) = up(i,j,k) - (drag_leaves_u*rdt)/(dx*rho_air)         ! averaged for gridspacing dx
@@ -226,7 +199,6 @@ module modtrees
                 end do
             end do
         end do
-        !write(6,* ) 'dragforce applied, move to excjs variables'
         ! E.g., synchronizing data across processors, call excjs, also for e12,thl,qt,svp possibly?
         call excjs(up,2,i1,2,j1,1,k1,ih,jh)
         call excjs(vp,2,i1,2,j1,1,k1,ih,jh)
@@ -235,11 +207,36 @@ module modtrees
         return
     end subroutine applytrees
 
+
+    subroutine drag_force_SFS_TKE(C_stem, A_stem, u1, v1, u2, v2, e120, drag_SFS) 
+        ! Drag force on SFS-TKE, take into account direction of surrounding windspeeds as SFS-TKE is a scalar at the cell-center
+        implicit none
+
+        ! input variables
+        real, intent(in) :: C_stem   ! Drag coefficient for stem
+        real, intent(in) :: A_stem   ! Area of stem
+        real, intent(in) :: u1,v1,u2,v2 ! velocity component
+        
+        ! output variables
+        real, intent(out) :: drag_SFS ! set to zero before call in applytrees
+
+        ! Local variables 
+        real :: u_mag ! magnitude of the velocity vector
+
+        ! Magnitude of the velocity vector at centre of gridcell
+        u_mag = 0.25*sqrt((u1+u2)**2 + (v1+v2)**2) !+ (w1+w2)**2)
+        
+        ! work performed by SFS motions against canopy drag (Patton et al. 2015)
+        drag_SFS = (8/3)*C_stem * A_stem * u_mag * e120
+
+    end subroutine drag_force_SFS_TKE
+
+
     !SvdL, 20231218: 3 dingen, (1) ik was verrast dat dit zo mocht met intent(out) drag_stem_u en niet declareren in enige outer scope? In C++ zou dat nooit mogen, dus niet geheel zeker over Fortran
     ! en (2) nu bereken je per i,j,k iteratie de kracht, maar dat houdt ook N^3 functiecalls in. Dat gebeurt in modibm ook, al vraag ik me af of dat inderdaad de beste optie is.. voor nu gewoon later zou ik zeggen.
     ! en (3) de snelheden zijn gegeven op de wanden van de cellen, dus de snelheid in het midden van de zelf is een middeling van deze snelheden. Dat zou eventueel nog geimplemteerd moeten worden (zie onder) 
     !SvdL, 20231218: heb de indentatie deels aangepast.
-    subroutine drag_force_stem(C_stem, A_stem, u1, v1, u2, v2, drag_stem_u, drag_stem_v)
+    subroutine drag_force_stem(C_stem, A_stem, u1, v1, u2, v2, drag_stem_u, drag_stem_v) ! Calculate drag in centre of cell in u and v direction
         implicit none
         
         ! Input variables
@@ -257,20 +254,13 @@ module modtrees
         u_mag = 0.25*sqrt((u1+u2)**2 + (v1+v2)**2) !+ (w1+w2)**2)
 
         ! Calculate the drag force components
-        drag_stem_u = C_stem * A_stem * 0.5 * (u1+u2) * u_mag
+        drag_stem_u = C_stem * A_stem * 0.5 * (u1+u2) * u_mag ! 0.5(u1+u2) -> avg in cell center
         drag_stem_v = C_stem * A_stem * 0.5 * (v1+v2) * u_mag
 
-        ! write(6,*) 'u_mag', u_mag
-        ! write(6,*) 'drag_u', drag_stem_u, 'drag_v', drag_stem_v
-        ! write(6,*) 'u1, u2', u1, u2
-        ! write(6,*) 'v1, v2', v1, v2
-        !write(6,*) 'w1, w2', w1, w2
         !SvdL, 20231218: deze heb ik uitgecommend: vanaf bovenaf gekeken is A_stem niet relevant, maar waarschijnlijk een veel kleiner oppervlak. Ook zal w zelf erg klein zijn.
-        ! drag_stem_w = -C_stem * A_stem * w * u_mag
-        !write(6,* ) 'dragforce calculated'
-     end subroutine drag_force_stem
-    
-    
+        !drag_stem_w = -C_stem * A_stem * w * u_mag
+    end subroutine drag_force_stem
+
 end module modtrees
 
 
