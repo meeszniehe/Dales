bff1d2cca37b35c3697b2310d5f3c5d06d5b864a
meeszniehe <meesz@niehe.com>
2024-04-23 12:59:01 +0200

    stemheight 3, crown 3x3xheight

--- src/modtrees.f90
+++ src/modtrees.f90
@@ -103,37 +103,37 @@ module modtrees
                 do k=1,kmax !
                     if(zf(k).LE.tree_height(i+myidx*imax,j+myidy*jmax)) then  ! obstacle height is above mid point of vertical grid
                         !!! Tree is represented by one straight line going straight up !!!!
-                        ltree_stem(i,j,k) = .true.     ! true/false array to indicate stem cells
-                        kindex_stem(i,j)   = k + 1     ! werkt niet voor overhangende bladeren/takken
-                        write(6,*) 'ltree_stem',i+myidx*imax,j+myidy*jmax,i,j,k,ltree_stem(i,j,k),tree_height(i+myidx*imax,j+myidy*jmax),zh(kindex_stem(i,j))
+                        ! ltree_stem(i,j,k) = .true.     ! true/false array to indicate stem cells
+                        ! kindex_stem(i,j)   = k + 1     ! werkt niet voor overhangende bladeren/takken
+                        ! write(6,*) 'ltree_stem',i+myidx*imax,j+myidy*jmax,i,j,k,ltree_stem(i,j,k),tree_height(i+myidx*imax,j+myidy*jmax),zh(kindex_stem(i,j))
                         
-                        ! !!!! Divide tree in smaller stem and thicker crown, same C_d value still !!!!
-                        ! if (k <= 3) then ! For the lowest two levels, thickness is 1 grid cell
-                        !     startIdx = 0
-                        !     endIdx = 0
-                        ! else ! For higher levels, thickness is 3 grid cells
-                        !     startIdx = -1
-                        !     endIdx = 1
-                        ! endif 
+                        !!!! Divide tree in smaller stem and thicker crown, same C_d value still !!!!
+                        if (k <= 4) then ! For the lowest two levels, thickness is 1 grid cell
+                            startIdx = 0
+                            endIdx = 0
+                        else ! For higher levels, thickness is 3 grid cells
+                            startIdx = -1
+                            endIdx = 1
+                        endif 
 
-                        ! do di = startIdx, endIdx ! if both are zero, no loop is executed
-                        !     do dj = startIdx, endIdx
-                        !         ! check whether crown stays within domain of (2-imax, 2-jmax) and move tree inward if not
-                        !         ! Is this needed, or does exjcs makes sure ltree=true value is send to adjecent processors?
-                        !         ! if ((i+di == 1) .OR. (j+dj == 1)) then 
-                        !         !     i = i + 1
-                        !         !     j = j + 1
-                        !         ! elseif ((i+di > imax) .OR. (j+dj > jmax)) then 
-                        !         !     i = i - 1
-                        !         !     j = j - 1    
-                        !         ! endif
-                        !         ltree_stem(i+di,j+dj,k) = .true.
-                        !         write(6,*) 'ltree_stem', ltree_stem(i+di,j+dj,k), i+myidx*imax,j+myidy*jmax,i,j,k,i+di,j+dj,tree_height(i+myidx*imax,j+myidy*jmax),zh(kindex_stem(i,j))
-                        !         if (di == 0 .AND. dj == 0) then ! Only update kindex_stem for the central cell
-                        !             kindex_stem(i,j) = k + 1
-                        !         endif
-                        !     end do
-                        ! end do
+                        do di = startIdx, endIdx ! if both are zero, no loop is executed
+                            do dj = startIdx, endIdx
+                                ! check whether crown stays within domain of (2-imax, 2-jmax) and move tree inward if not
+                                ! Is this needed, or does exjcs makes sure ltree=true value is send to adjecent processors?
+                                ! if ((i+di == 1) .OR. (j+dj == 1)) then 
+                                !     i = i + 1
+                                !     j = j + 1
+                                ! elseif ((i+di > imax) .OR. (j+dj > jmax)) then 
+                                !     i = i - 1
+                                !     j = j - 1    
+                                ! endif
+                                ltree_stem(i+di,j+dj,k) = .true.
+                                write(6,*) 'ltree_stem', ltree_stem(i+di,j+dj,k), i+myidx*imax,j+myidy*jmax,i,j,k,i+di,j+dj,tree_height(i+myidx*imax,j+myidy*jmax),zh(kindex_stem(i,j))
+                                if (di == 0 .AND. dj == 0) then ! Only update kindex_stem for the central cell
+                                    kindex_stem(i,j) = k + 1
+                                endif
+                            end do
+                        end do
 
                     endif
                 end do  !k
