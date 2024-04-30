7413a562e0c31ac9d157b246192945a078c7d41b
meeszniehe <meesz@niehe.com>
2024-04-30 16:51:13 +0200

    same bug

--- src/modtrees.f90
+++ src/modtrees.f90
@@ -194,7 +194,7 @@ module modtrees
     
         ! Declare local variables
         integer :: i, j, k
-        real :: drag_stem_u, drag_stem_v, drag_SFS
+        real :: drag_stem_u, drag_stem_v, drag_SFS, A_padk
 
         if (.not. lapply_trees) return
     
@@ -203,11 +203,12 @@ module modtrees
             do j=2,j1
                 do k=1,kmax                 
                     if(ltree_stem(i,j,k)) then   ! could be faster by limiting k to highest tree value?
-                        write(6,*) 'ltree is true for index with A_pad', i, j, k, A_pad(k)                 
+                        A_padk = A_pad(k)
+                        write(6,*) 'ltree is true for index with A_pad', i, j, k, A_padk                
                         ! Drag on resolved TKE due to 
                         drag_stem_u = 0 
                         drag_stem_v = 0
-                        call drag_force_stem(C_stem, A_pad(k), u0(i-1,j,k), v0(i,j-1,k), u0(i,j,k), v0(i,j,k), drag_stem_u, drag_stem_v)
+                        call drag_force_stem(C_stem, A_padk, u0(i-1,j,k), v0(i,j-1,k), u0(i,j,k), v0(i,j,k), drag_stem_u, drag_stem_v)
                         ! Reassign the velocity value at the faces adjusted for drag in u and v direction
                         up(i-1,j,k) = up(i-1,j,k) - drag_stem_u/2        ! both sides get half the drag calculated from the middle. 
                         up(i,j,k) = up(i,j,k) - drag_stem_u/2  
@@ -216,8 +217,8 @@ module modtrees
 
                         ! Drag on SFS-TKE
                         drag_SFS = 0
-                        call drag_force_SFS_TKE(C_stem, A_pad(k), u0(i-1,j,k), v0(i,j-1,k), u0(i,j,k), v0(i,j,k), e120(i,j,k), drag_SFS) ! e120?? 
-                        ! use square of e12 or not?
+                        call drag_force_SFS_TKE(C_stem, A_padk, u0(i-1,j,k), v0(i,j-1,k), u0(i,j,k), v0(i,j,k), e120(i,j,k), drag_SFS) ! e120?? 
+                        ! use square of e12 or not
                         write(6,*) e12p(i,j,k)
                         e12p(i,j,k) = e12p(i,j,k) - drag_SFS
                         write(6,*) drag_SFS, e12p(i,j,k), e120(i,j,k)
