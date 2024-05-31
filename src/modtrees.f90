b62fc5b0aee9742af2379062f6574e3f03119f4e
meeszniehe <meesz@niehe.com>
2024-05-31 09:09:31 +0200

    k=>2, cause k=1 doesnt work, output explode

--- src/modtrees.f90
+++ src/modtrees.f90
@@ -160,7 +160,7 @@ module modtrees
                         vp(i,j,k) = vp(i,j,k) + treedrag_v/2
 
                         !!! Source for SGS-TKE !!!
-                        if (lapply_sourceSGS) then
+                        if (lapply_sourceSGS .AND. (k >= 2)) then
                             source_SGS = 0
                             call source_SGS_TKE(Cd, A_pad, u0(i-1, j, k), v0(i, j-1, k), u0(i, j, k), v0(i, j, k), e120(i, j, k), source_SGS)
                             ! write(6,*) e12p(i, j, k)
